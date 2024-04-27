ac976d81dbc7f3bec8fcd488e5ae9601f4669c4b
Eswin Poroj <eporojc1@miumg.edu.gt>
2024-04-26 22:51:31 -0600

    Agregue la opcion de ver Saldo de Clientes

--- cajeroAutomatico.dart
+++ cajeroAutomatico.dart
@@ -54,4 +54,29 @@ class Clientes extends CajeroAutomatico {
       }
     });
   }
+
+  void motrarDatosClientes2(String cedula) {
+    for (var datos in datosClientesExistentes) {
+      if (datos['cedula'] == cedula) {
+        datos = datos.toString();
+        print(datos);
+      }
+    }
+  }
+}
+
+void main(List<String> args) {
+  var cliente = Clientes([]);
+
+  cliente.mostrarDatosClientes("1234567890");
+  cliente.agregarCliente({
+    "nombre": "Carlos",
+    "apellido": "Gomez",
+    "cedula": "3456789012",
+    "direccion": "Calle 4",
+    "telefono": "7890123456",
+    "correo": "carlosGomez43@gmail.com",
+    "contrasena": "1234",
+    "saldo": "4000"
+  });
 }
